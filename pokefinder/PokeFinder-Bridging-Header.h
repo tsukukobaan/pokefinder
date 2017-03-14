3fff0d77dfd1f54f3652a5d08dadb5697cd66fb6
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-03-14 23:06:08 +0900

    first commit but almost done

---
+++ pokefinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  pokefinder
+//
+//  Created by 小林 泰 on 2017/03/14.
+//  Copyright © 2017年 TokyoIceHockeyChannel. All rights reserved.
+//
+
+#import "GeoFire.h"
